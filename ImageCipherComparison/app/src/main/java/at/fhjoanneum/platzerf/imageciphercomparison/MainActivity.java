fb60d723443005063edf88f6bdd004dc4cbd4033
Franz Platzer <franz.platzer@acp.at>
2017-06-07 15:23:05 +0200

    running in sigsegv

--- ImageCipherComparison/app/src/main/java/at/fhjoanneum/platzerf/imageciphercomparison/MainActivity.java
+++ ImageCipherComparison/app/src/main/java/at/fhjoanneum/platzerf/imageciphercomparison/MainActivity.java
@@ -1,8 +1,12 @@
 package at.fhjoanneum.platzerf.imageciphercomparison;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,10 +15,40 @@ import android.widget.TextView;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
 
     private TextView upperText;
     private TextView lowerText;
@@ -30,6 +64,11 @@ public class MainActivity extends AppCompatActivity {
 
     private int[] encryptedImageBytes;
 
+    private String filename = "/sdcard/Download/Lenna.jpg";
+    private int height;
+    private int width;
+    private Bitmap.Config config;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,27 +77,9 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         upperText = (TextView) findViewById(R.id.sample_text);
 
-        Bitmap image = BitmapFactory.decodeFile("/sdcard/Download/2x3image.jpg");
+        verifyStoragePermissions(this);
 
-        int[] pixels = new int[image.getHeight()*image.getWidth()*3];
-
-        int pos = 0;
-        for(int y = 0; y < image.getHeight(); y++) {
-            for(int x = 0; x < image.getWidth(); x++) {
-                int pixel = image.getPixel(x, y);
-                pixels[pos*3] = Color.red(pixel);
-                pixels[pos*3+1] = Color.green(pixel);
-                pixels[pos*3+2] = Color.blue(pixel);
-                pos++;
-            }
-        }
-
-        originalImageBytes = pixels;
-        encryptedImageBytes = new int[originalImageBytes.length];
-
-        for(int i = 0; i < originalImageBytes.length; i++){
-            sumOfImageBytes += originalImageBytes[i];
-        }
+        loadImageIntoRgbIntArray(filename);
 
         upperText.setText("Original Image Bytes: " + arrayToStringList(originalImageBytes));
 
@@ -66,10 +87,73 @@ public class MainActivity extends AppCompatActivity {
         lowerText.setText("do something...");
     }
 
-    void
+    void saveRgbIntArrayToImage(int[] convertedImage, String newFilename) {
+
+        Bitmap image = Bitmap.createBitmap(width, height, config);
+
+        int pos = 0;
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                image.setPixel(
+                    x,
+                    y,
+                    Color.rgb(
+                        convertedImage[pos*3],
+                        convertedImage[pos*3+1],
+                        convertedImage[pos*3+2])
+                );
+                pos++;
+            }
+        }
+
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(newFilename);
+            image.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
+            // PNG is a lossless format, the compression factor (100) is ignored
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void loadImageIntoRgbIntArray(String path){
+        Bitmap image = BitmapFactory.decodeFile(path);
+
+        config = image.getConfig();
+        height = image.getHeight();
+        width = image.getWidth();
+        originalImageBytes = new int[image.getHeight()*image.getWidth()*3];
+
+        int pos = 0;
+        for(int y = 0; y < image.getHeight(); y++) {
+            for(int x = 0; x < image.getWidth(); x++) {
+                int pixel = image.getPixel(x, y);
+                originalImageBytes[pos*3] = Color.red(pixel);
+                originalImageBytes[pos*3+1] = Color.green(pixel);
+                originalImageBytes[pos*3+2] = Color.blue(pixel);
+                pos++;
+            }
+        }
+
+        encryptedImageBytes = new int[originalImageBytes.length];
+
+        for(int i = 0; i < originalImageBytes.length; i++){
+            sumOfImageBytes += originalImageBytes[i];
+        }
+    }
 
     public void onDecrypt(View v) {
         int[] decryptImageBytes = decryptImageBytes(encryptedImageBytes, sumOfImageBytes);
+
+        saveRgbIntArrayToImage(decryptImageBytes, filename + ".decrypted.jpg");
 
         lowerText.setText("Decrypted successfully: " + arrayToStringList(decryptImageBytes));
     }
@@ -85,6 +169,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void onEncrypt(View v) {
         encryptedImageBytes = encryptImageBytes(originalImageBytes, sumOfImageBytes);
+
+        saveRgbIntArrayToImage(encryptedImageBytes, filename + ".encrypted.jpg");
 
         lowerText.setText("Encrypted successfully: " + arrayToStringList(encryptedImageBytes));
     }
